480d9c029ee7923a45b97c16a591d23b0878a710
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-12-16 13:02:10 +0530

    add global styles

---
+++ styles/global.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  titleText: {
+    fontFamily: "ArchitectsDaughter_400Regular",
+    fontSize: 18,
+    color: "#333",
+  },
+  container: {
+    // flex: 1,
+    padding: 20,
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
