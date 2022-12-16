9c3a4b89ed895fd8713ccc9e85029f9b0200d3ce
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-12-16 12:57:09 +0530

    started a new react native project

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import DrawerNavigator from "./routes/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  ArchitectsDaughter_400Regular,
+  useFonts,
+} from "@expo-google-fonts/architects-daughter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    ArchitectsDaughter_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
